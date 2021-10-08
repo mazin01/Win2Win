691a5ddcd6bb4cd775c4898bd742e8c3f8fea888
mazin01 <mmmazin01@gmail.com>
2021-10-08 17:19:20 +0200

    get data :/

--- src/App.js
+++ src/App.js
@@ -2,25 +2,60 @@ import React, { useState } from "react";
 import Signin from "./components/signin";
 import { Link } from "react-router-dom"
 import HomePage from "./components/homePage";
-import Axios from "axios";
+import axios from "axios";
 
 function App() {
-  const data = '{\r\n    "email": "mohamedaldie@gmail.com",\r\n    "password": "P@$$w0rd"\r\n}';
-  const config = {
-    method: 'post',
-    url: 'https://w2wworld.herokuapp.com/api/v1/accounts/w2w-user/authenticate',
-    headers: { },
-    data : data
-  };
+  // const data = '{\r\n    "email": "mohamedaldie@gmail.com",\r\n    "password": "P@$$w0rd"\r\n}';
+  // const config = {
+  //   method: 'post',
+  //   url: 'https://w2wworld.herokuapp.com/api/v1/accounts/w2w-user/authenticate',
+  //   headers: { },
+  //   data : data
+  // };
   
-  Axios(config)
-  .then(function (response) {
-    console.log(JSON.stringify(response.data));
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+  // Axios(config)
+  // .then(function (response) {
+  //   console.log(JSON.stringify(response.data));
+  // })
+  // .catch(function (error) {
+  //   console.log(error);
+  // });
 
+const token = "<token>"
+
+const config = {
+  method: 'get',
+  url: 'https://w2wworld.herokuapp.com/api/v1/all_subscribers',
+  headers: { 
+    'Server': 'Cowboy',
+    'Connection': 'keep-alive',
+    'Vary': 'Origin, Accept-Encoding',
+    'Access-Control-Allow-Credentials': 'true',
+    'X-Dns-Prefetch-Control': 'off',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Strict-Transport-Security': 'max-age=15552000; includeSubDomains',
+    'X-Download-Options': 'noopen',
+    'X-Content-Type-Options': 'nosniff',
+    'X-Xss-Protection': '1; mode=block',
+    'X-Ratelimit-Limit': '1000',
+    'X-Ratelimit-Remaining': '999',
+    'Date': 'Wed, 15 Sep 2021 13:57:59 GMT',
+    'X-Ratelimit-Reset': '1631714280',
+    'Content-Type': 'application/json; charset=utf-8',
+    'Etag': 'W/"28b51-xivqaev/tJoMHhWN54R4Xp8pDsA"',
+    'Content-Encoding': 'gzip',
+    'Transfer-Encoding': 'chunked',
+    'Via': '1.1 vegur',
+   }
+};
+
+axios(config)
+.then(function (resp) {
+  console.log(JSON.stringify(resp.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
 
 
   const adminUser = {
@@ -54,7 +89,7 @@ function App() {
 
   return (
     <div className="App">
-      <p>{}</p>
+      {console.log()}
       {(user.email != "") ? (
         <HomePage user={user} logout={logout} />
       ) : (
